139f8274994efb9d6f2cc952f9fe3855a5af9009
imranpopz <imranpopz32@gmail.com>
2019-11-03 20:50:34 +0530

    Update patch path

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,6 +4,7 @@ cd system/core
 patch -p1 < ../../device/huawei/h30u10/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 patch -p1 < ../../device/huawei/h30u10/patches/earlysuspend.patch
 patch -p1 < ../../device/huawei/h30u10/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
+patch -p1 < ../../device/huawei/h30u10/patches/0001-liblog-Add-__xlog_buf_printf.patch
 cd ../..
 cd bionic
 patch -p1 < ../device/huawei/h30u10/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
@@ -11,6 +12,6 @@ cd ..
 cd hardware/ril
 patch -p1 < ../../device/huawei/h30u10/patches/0003-BOARD_PROVIDES_RILD.patch
 cd ../..
-cd frameworks/opt/telephony
-patch -p1 < ../../device/huawei/h30u10/patches/0005-RIL.java-add-mtk-ril-requests-unsol-responses.patch
-cd ../../..
+cd hardware/interfaces
+patch -p1 < ../../device/huawei/h30u10/patches/sensor.patch
+cd ../..
