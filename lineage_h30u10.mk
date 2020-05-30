25255f61bee6f4f9d6338658193f0b5f584c31d0
imranpopz <imranpopz32@gmail.com>
2020-05-30 13:45:56 +0530

    Build graphic lib from source

--- lineage_h30u10.mk
+++ lineage_h30u10.mk
@@ -135,14 +135,18 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf
-	
+
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.renderscript@1.0-impl \
+    libgralloc_extra \
+    libui_ext \
+    libgui_ext
 
 # Vibrator
 PRODUCT_PACKAGES += \
