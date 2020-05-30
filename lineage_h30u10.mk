1281c44bf13365733c9c68980db189d416d2ee1f
imranpopz <imranpopz32@gmail.com>
2020-05-30 14:12:51 +0530

    Build libcam.utils.sensorlistener

--- lineage_h30u10.mk
+++ lineage_h30u10.mk
@@ -152,7 +152,8 @@ PRODUCT_PACKAGES += \
 # Camera HAL
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl-legacy \
-    libcam.client
+    libcam.client \
+    libcam.utils.sensorlistener
 
 # Vibrator
 PRODUCT_PACKAGES += \
