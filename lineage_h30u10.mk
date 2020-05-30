8ed534b0511d70e19864e688bab0773fa5bea72b
imranpopz <imranpopz32@gmail.com>
2020-05-30 14:04:27 +0530

    Build libcam.client

--- lineage_h30u10.mk
+++ lineage_h30u10.mk
@@ -151,7 +151,8 @@ PRODUCT_PACKAGES += \
 
 # Camera HAL
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy
+    android.hardware.camera.provider@2.4-impl-legacy \
+    libcam.client
 
 # Vibrator
 PRODUCT_PACKAGES += \
