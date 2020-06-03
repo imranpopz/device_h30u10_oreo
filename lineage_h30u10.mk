b794b59e29008f5d44ec535fee27b59b9750ae8e
imranpopz <imranpopz32@gmail.com>
2020-06-03 23:40:04 +0530

    Copy seccomp to vendor

--- lineage_h30u10.mk
+++ lineage_h30u10.mk
@@ -186,13 +186,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
 
-
+# Seccomp
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/_hals.conf:system/vendor/etc/sensors/_hals.conf
-
-# Inherit common Android Go configurations
-$(call inherit-product, build/target/product/go_defaults.mk)
 
 # call the proprietary setup
 $(call inherit-product, vendor/huawei/h30u10/h30u10-vendor.mk)
