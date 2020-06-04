cdb79cec014f6ff881461b3b42b8d82a33608449
imranpopz <imranpopz32@gmail.com>
2020-06-04 10:02:13 +0530

    Update shims from xcore995

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -3,26 +3,15 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-	ril.cpp \
-	GraphicBuffer.cpp \
-	icu55.c \
-	bionic.cpp \
-	wvm.cpp \
-	atomic.cpp
+    bionic.cpp \
+    xlog.c \
+    ui.cpp \
+    icu.c \
+    camera.cpp \
+    buffer.cpp
 
-
-LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-LOCAL_SHARED_LIBRARIES := libbinder libutils liblog libgui libui \
-				libicuuc libicui18n libcrypto libmedia libstagefright_foundation
-
+LOCAL_SHARED_LIBRARIES := libbinder libui libgui libcutils libutils libc liblog libicuuc libicui18n  libstagefright_foundation
 LOCAL_MODULE := libshim
 LOCAL_MODULE_TAGS := optional
-LOCAL_CLANG := true
-LOCAL_CPPFLAGS := -std=c++1y
-# LOCAL_SANITIZE := integer
-LOCAL_CPPFLAGS += -Wno-exit-time-destructors
-LOCAL_CPPFLAGS += -Wno-global-constructors
-LOCAL_CPPFLAGS += -Wno-c++98-compat-pedantic
-LOCAL_CPPFLAGS += -Wno-four-char-constants
-LOCAL_CPPFLAGS += -Wno-padded
+
 include $(BUILD_SHARED_LIBRARY)
--- libshims/icu.c
+++ libshims/icu.c
@@ -1,34 +1,18 @@
-/*
- * Copyright (C) 2017 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include "unicode/ucnv.h"
 
 U_STABLE UConverter* U_EXPORT2
-ucnv_open_55(const char *converterName, UErrorCode *err)
+ucnv_open_56(const char *converterName, UErrorCode *err)
 {
     return ucnv_open(converterName, err);
 }
 
 U_STABLE void  U_EXPORT2
-ucnv_close_55(UConverter * converter)
+ucnv_close_56(UConverter * converter)
 {
     return ucnv_close(converter);
 }
 
-U_STABLE void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP_55(
+U_STABLE void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP_56(
                   const void *context,
                   UConverterToUnicodeArgs *toUArgs,
                   const char* codeUnits,
@@ -39,7 +23,7 @@ U_STABLE void U_EXPORT2 UCNV_TO_U_CALLBACK_STOP_55(
     UCNV_TO_U_CALLBACK_STOP(context, toUArgs, codeUnits, length, reason, err);
 }
 
-U_STABLE void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP_55(
+U_STABLE void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP_56(
                   const void *context,
                   UConverterFromUnicodeArgs *fromUArgs,
                   const UChar* codeUnits,
@@ -52,7 +36,7 @@ U_STABLE void U_EXPORT2 UCNV_FROM_U_CALLBACK_STOP_55(
 }
 
 U_STABLE void U_EXPORT2
-ucnv_setToUCallBack_55(UConverter * converter,
+ucnv_setToUCallBack_56(UConverter * converter,
                      UConverterToUCallback newAction,
                      const void* newContext,
                      UConverterToUCallback *oldAction,
@@ -63,7 +47,7 @@ ucnv_setToUCallBack_55(UConverter * converter,
 }
 
 U_STABLE void U_EXPORT2
-ucnv_setFromUCallBack_55(UConverter * converter,
+ucnv_setFromUCallBack_56(UConverter * converter,
                        UConverterFromUCallback newAction,
                        const void *newContext,
                        UConverterFromUCallback *oldAction,
@@ -74,7 +58,7 @@ ucnv_setFromUCallBack_55(UConverter * converter,
 }
 
 U_STABLE void U_EXPORT2
-ucnv_convertEx_55(UConverter *targetCnv, UConverter *sourceCnv,
+ucnv_convertEx_56(UConverter *targetCnv, UConverter *sourceCnv,
                char **target, const char *targetLimit,
                const char **source, const char *sourceLimit,
                UChar *pivotStart, UChar **pivotSource,
