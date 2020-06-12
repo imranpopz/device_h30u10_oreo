1b7e2de81fcd6b4f77d05f84c615295e681fc197
imranpopz <imranpopz32@gmail.com>
2020-06-12 08:47:54 +0530

    Update patches for pie

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -2,16 +2,13 @@
 cd ../../../..
 cd system/core
 patch -p1 < ../../device/huawei/h30u10/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
-patch -p1 < ../../device/huawei/h30u10/patches/earlysuspend.patch
+patch -p1 < ../../device/huawei/h30u10/patches/0004-libsuspend-readd-earlysuspend.patch
 patch -p1 < ../../device/huawei/h30u10/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
 patch -p1 < ../../device/huawei/h30u10/patches/0001-liblog-Add-__xlog_buf_printf.patch
 cd ../..
 cd bionic
 patch -p1 < ../device/huawei/h30u10/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
 cd ..
-cd hardware/ril
-patch -p1 < ../../device/huawei/h30u10/patches/0003-BOARD_PROVIDES_RILD.patch
-cd ../..
 cd hardware/interfaces
 patch -p1 < ../../device/huawei/h30u10/patches/sensor.patch
 cd ../..
